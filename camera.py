7a6697cc58f58b00316f68c90fb10b605e79ec6e
ParvYadav <parvyadav2011@gmail.com>
2022-03-09 18:24:33 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -34,6 +34,8 @@ class VideoCamera(object):
     
     detector = dlib.get_frontal_face_detector()
 #     predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks (2).dat')
+
+    sequence_count_frame = 0
     
     def process_one(self):
         if not self.to_process:
@@ -52,9 +54,32 @@ class VideoCamera(object):
         
         
         
+        sequence_count_frame += 1
+        small_frame = cv2.resize(bgr_image, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_names = []
         
-
-        
+        gray = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+        rects = detector(gray, 0)
+        for rect in rects:
+            x = rect.left()
+            y = rect.top()
+            x1 = rect.right()
+            y1 = rect.bottom()
+            
+            landmarks = np.matrix([[p.x, p.y] for p in predictor(frame, rect).parts()])
+            # get the left eye landmarks
+            left_eye = landmarks[LEFT_EYE_POINTS]
+            # get the right eye landmarks
+            right_eye = landmarks[RIGHT_EYE_POINTS]
+            # draw contours on the eyes
+            left_eye_hull = cv2.convexHull(left_eye)
+            right_eye_hull = cv2.convexHull(right_eye)
+            cv2.drawContours(bgr_image, [left_eye_hull], -1, (0, 255, 0),
+                             1)  # (image, [contour], all_contours, color, thickness)
+            cv2.drawContours(bgr_image, [right_eye_hull], -1, (0, 255, 0), 1)
         # _______________________________________Performing some pre processing_______________________________________________
         
         cv2.putText(rgb_image, "Winked {}".format('1'), (10, 30), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 0),1)
